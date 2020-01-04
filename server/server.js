27177cb814ba4f758fa6407f302ee5d5e0d38ab0
Taylor Rodrigues <taylor.d.rod33@gmail.com>
2020-01-03 21:34:38 -0800

    initial boilerplate

---
+++ server/server.js
@@ -0,0 +1,55 @@
+const path = require('path');
+const express = require('express');
+
+const app = express();
+const PORT = 3000;
+
+/**
+ * handle parsing request body
+ */
+app.use(express.json()) // for parsing application/json
+app.use(express.urlencoded({ extended: true })) // for parsing application/x-www-form-urlencoded
+
+/**
+ * handle requests for static files
+ */
+app.use('/assets', express.static(path.join(__dirname, './assets')));
+
+/**
+ * define route handlers
+ */
+
+// if production
+if (process.env.NODE_ENV === 'production') {
+  // statically serve everything in the build folder on the route '/build'
+  app.use('/build', express.static(path.join(__dirname, '../build')));
+  // serve index.html on the route '/'
+  app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, '../index.html'));
+  });
+}
+
+// respond with main app
+app.get('/*', (req, res) => res.status(200).sendFile(path.resolve(__dirname, '../client/index.html')));
+
+// catch-all route handler for any requests to an unknown route
+app.use((req, res) => res.sendStatus(404));
+
+// error handler
+app.use((err, req, res, next) => {
+  const defaultErr = {
+    log: 'Express error handler caught unknown middleware error',
+    status: 400,
+    message: { err: 'An error occurred' },
+  };
+  const errorObj = Object.assign({}, defaultErr, err);
+  console.log(errorObj.log);
+  return res.status(errorObj.status).json(errorObj.message);
+});
+
+// start server
+app.listen(PORT, () => {
+  console.log(`Server listening on port: ${PORT}...`)
+})
+
+module.exports = app;
