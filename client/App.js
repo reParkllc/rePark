270911aa6411fc3717d816d8931db158945bba32
Arman Gurkan <arman@secelektrik.com>
2020-01-04 19:38:43 -0800

    I added the rotating carousel not edited anything

--- client/App.js
+++ client/App.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import LogInComponent from "./components/LogInComponent/LogInComponent";
+import RtOnboard from "./components/RtOnboard/RtOnboard";
 
 const App = () => {
   return (
     <div>
-        <LogInComponent/>
+        <RtOnboard/>
     </div>
   )
 };
