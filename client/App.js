f0c362143d52389dfdf39708e39151f37161e6d7
Taylor Rodrigues <taylor.d.rod33@gmail.com>
2020-01-06 14:58:16 -0800

    signup close to finished

--- client/App.js
+++ client/App.js
@@ -1,26 +1,37 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import LogInComponent from './components/LogInComponent/LogInComponent';
-import SignUpInitial from './containers/SignUp/SignUpInitial';
-import SignUpSecondary from './containers/SignUp/SignUpSecondary';
+import SignUpInitial from './components/SignUpInitialComponent/SignUpInitial';
+import SignUpSecondary from './components/SignUpSecondaryComponent/SignUpSecondary';
 import MapComponent from './components/MapComponent/MapComponent';
+import OnboardingComponent from "./components/OnboardingComponent/OnboardingComponent";
+import { UserContext } from './contexts/UserContext';
+import MainPage from './components/MainPage/MainPage';
 import {
   BrowserRouter as Router,
   Switch,
+  Redirect,
   Route,
   Link
 } from "react-router-dom";
-import OnboardingComponent from "./components/OnboardingComponent/OnboardingComponent";
 
 //TODO: Add "/" route and corresponding component as a splash page for users who are not logged in
 //TODO: Add a route for the actual app once a user has logged in
 //TODO: Add a route for the users profile page
 
 const App = () => {
+  const { user } = useContext(UserContext)
   return (
     <Router>
       <Switch>
         <Route
+          exact
           path="/"
+          component={MainPage}
+        >
+          {/* {user.isLoggedIn ? <Redirect to="/main" /> : <Redirect to="/welcome" />} */}
+        </Route>
+        <Route
+          path="/main"
           exact
           component={MapComponent}
         />
@@ -41,9 +52,9 @@ const App = () => {
         />
         //TODO: I inserted a temporary path actually another prop should be passed as isLogged in
         <Route
-            path="/welcome"
-            exact
-            component={OnboardingComponent}
+          path="/welcome"
+          exact
+          component={OnboardingComponent}
         />
       </Switch>
     </Router>
--- client/components/OnboardingComponent/OnboardingComponent.js
+++ client/components/OnboardingComponent/OnboardingComponent.js
@@ -1,48 +1,51 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { AutoRotatingCarousel, Slide } from 'material-auto-rotating-carousel'
 import CssBaseline from '@material-ui/core/CssBaseline';
-import { green400, green600, blue400, blue600, red400, red600 } from '@material-ui/core/colors'
-import { useTheme } from '@material-ui/core/styles'
+import { green400, green600 } from '@material-ui/core/colors/green';
+import { blue400, blue600 } from '@material-ui/core/colors/blue';
+import { red400, red600 } from '@material-ui/core/colors/red';
+import { useTheme } from '@material-ui/core/styles';
 import useMediaQuery from "@material-ui/core/useMediaQuery";
-import {Link} from 'react-router-dom'
 
-const OnboardingComponent = props => {
-    const theme = useTheme();
-    const matches = useMediaQuery(theme.breakpoints.down('sm'));
-    return (
-        <div>
-            <CssBaseline/>
-            <Link to={'./LogInComponent'}>
-                <AutoRotatingCarousel
-                    label="Get started"
-                    open
-                    mobile = {matches}
-                >
-                    <Slide
-                        media={<img src="http://www.icons101.com/icon_png/size_256/id_79394/youtube.png" />}
-                        mediaBackgroundStyle={theme.palette.secondary}
-                        contentStyle={{ backgroundColor: red600 }}
-                        title="This is a very cool feature"
-                        subtitle="Just using this will blow your mind."
-                    />
-                    <Slide
-                        media={<img src="http://www.icons101.com/icon_png/size_256/id_80975/GoogleInbox.png" />}
-                        mediaBackgroundStyle={{ backgroundColor: blue400 }}
-                        contentStyle={{ backgroundColor: blue600 }}
-                        title="Ever wanted to be popular?"
-                        subtitle="Well just mix two colors and your are good to go!"
-                    />
-                    <Slide
-                        media={<img src="http://www.icons101.com/icon_png/size_256/id_76704/Google_Settings.png" />}
-                        mediaBackgroundStyle={{ backgroundColor: green400 }}
-                        contentStyle={{ backgroundColor: green600 }}
-                        title="May the force be with you"
-                        subtitle="The Force is a metaphysical and ubiquitous power in the Star Wars universe."
-                    />
-                </AutoRotatingCarousel>
-            </Link>
-        </div>
-    )
+
+const OnboardingComponent = (props) => {
+  const theme = useTheme();
+  const matches = useMediaQuery(theme.breakpoints.down('sm'));
+  const [handleOpen, setHandleOpen] = useState(true);
+  return (
+    <div>
+      <CssBaseline />
+      <AutoRotatingCarousel
+        label="Get started"
+        open={handleOpen}
+        onClose={() => setHandleOpen(false)}
+        onStart={() => setHandleOpen(false)}
+        mobile={matches}
+      >
+        <Slide
+          media={<img src="http://www.icons101.com/icon_png/size_256/id_79394/youtube.png" />}
+          mediaBackgroundStyle={theme.palette.secondary}
+          contentstyle={{ backgroundColor: red600 }}
+          title="This is a very cool feature"
+          subtitle="Just using this will blow your mind."
+        />
+        <Slide
+          media={<img src="http://www.icons101.com/icon_png/size_256/id_80975/GoogleInbox.png" />}
+          mediaBackgroundStyle={{ backgroundColor: blue400 }}
+          contentstyle={{ backgroundColor: blue600 }}
+          title="Ever wanted to be popular?"
+          subtitle="Well just mix two colors and your are good to go!"
+        />
+        <Slide
+          media={<img src="http://www.icons101.com/icon_png/size_256/id_76704/Google_Settings.png" />}
+          mediaBackgroundStyle={{ backgroundColor: green400 }}
+          contentstyle={{ backgroundColor: green600 }}
+          title="May the force be with you"
+          subtitle="The Force is a metaphysical and ubiquitous power in the Star Wars universe."
+        />
+      </AutoRotatingCarousel>
+    </div >
+  )
 };
 
 export default OnboardingComponent;
