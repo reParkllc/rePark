5f57bbd3abcce76e78acf91c9d7d8147ccad0ce0
Taylor Rodrigues <taylor.d.rod33@gmail.com>
2020-01-06 15:53:12 -0800

    fixed saving user to db

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -3,52 +3,53 @@ const { User } = require('../models/userModels');
 
 const userController = {
 
-//Create user controller
-  createUser (req, res, next) {
-	const { name, phone, pass } = req.body;
-	const newUser = {
-		name,
-		phone,
-    pass
-	}
+  //Create user controller
+  createUser(req, res, next) {
+    const { name, phone, pass } = req.body;
+    const phoneNum = Number(phone);
+    const newUser = {
+      name: name,
+      phone: phoneNum,
+      pass: pass
+    }
 
-	User.create(newUser)
-	.then(userDoc => {
-		res.locals.user = userDoc;
-		return next();
-	})
-	.catch(err => {
-		return next({
-				log: 'Express error handler caught user create error',
-				status: 400,
-				message: { err: 'An error occurred' },
-			});
-	});
-},
+    User.create(newUser)
+      .then(userDoc => {
+        res.locals.user = userDoc;
+        return next();
+      })
+      .catch(err => {
+        return next({
+          log: 'Express error handler caught user create error',
+          status: 400,
+          message: { err: 'An error occurred' },
+        });
+      });
+  },
 
   //verify user controller
-  verifyUser (req, res, next) {
+  verifyUser(req, res, next) {
     const { name, pass } = req.body;
-		User.findOne({ name })
-		.exec()
-		.then(userDoc => {
-			if (userDoc.name === null) {
-        res.status(401).json({ err })
-				return next();
-			}
-			if (userDoc.pass === req.body.pass) {
-				res.locals.user = userDoc;
-				return next();
-			}
-		})
-		.catch(err => {
-			res.status(401).json({'Error': err});
-			return next({
-				log: 'ERROR: userController verifyUser error',
-				status: 400,
-				message: { err: 'An error occurred' },
-			});
-		});
-	}
+    User.findOne({ name })
+      .exec()
+      .then(userDoc => {
+        if (userDoc.name === null) {
+          res.status(401).json({ err })
+          return next();
+        }
+        if (userDoc.pass === req.body.pass) {
+          res.locals.user = userDoc;
+          return next();
+        }
+      })
+      .catch(err => {
+        res.status(401).json({ 'Error': err });
+        return next({
+          log: 'ERROR: userController verifyUser error',
+          status: 400,
+          message: { err: 'An error occurred' },
+        });
+      });
+  }
 }
 module.exports = userController;
