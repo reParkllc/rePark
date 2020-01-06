f6e20d7b84f96a27a8149720cd60b4988a5b5be9
Jie-Yun Cheng <jie-yuncheng@Jie-Yuns-MacBook-Pro.local>
2020-01-05 19:07:30 -0800

    added a map react component that uses MapBox api that has 2 hard-coded pins that can open a pop up window when clicked, and will display the coordinates

--- client/App.js
+++ client/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import LogInComponent from "./components/LogInComponent/LogInComponent";
+import MapComponent from './MapComponent.js/MapComponent';
 
 const App = () => {
   return (
     <div>This is a test
         <LogInComponent/>
+        <MapComponent />
     </div>
   )
 };
